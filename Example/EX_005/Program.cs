42a1e50501b792bbf07d2c840a62b8a09a37d503
Блажевич Дмитрий <dima1988_bl@mail.ru>
2021-10-26 05:24:33 +0300

    I5

--- Example/EX_005/Program.cs
+++ Example/EX_005/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "дима")
+{
+    Console.WriteLine("Привет красавчик!");
+}    
+else
+{
+    Console.Write("Привет!, ");
+    Console.WriteLine(username);
+}
+
